6b8adfb4b75f7d72caf1c08a802fe6364ee5f828
Whitney Armstrong <whit@temple.edu>
2010-11-04 22:35:11 -0400

    Working on geant4vmc implementation

---
+++ include/BETAG4Application.hh
@@ -0,0 +1,14 @@
+#ifndef BETAG4Application_HH
+#define BETAG4Application_HH
+#include <TVirtualMCApplication.h>
+
+class BETAG4Application : public TVirtualMCApplication {
+  BETAG4Application();
+  BETAG4Application(int argc,char** argv);
+  ~BETAG4Application();
+
+};
+
+
+#endif
+
